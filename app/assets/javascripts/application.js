48ed51e02bf4f63d34025a329c5b212ba6008453
wesr7 <jwesleyrodgers@gmail.com>
2015-12-01 22:25:26 -0800

    updated index page and added a small scroll effect using jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,39 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+
+
+// $(document).ready(function(){
+
+//     var animationSpeed = 1000;
+//     var pause = 5000;
+//     var currentSlide = 1;
+
+//     var $slider = $('#slider');
+//     var $sliderContainer = $slider.find('.slides');
+//     var $slides = $slider.find('.slide');
+
+
+//     var interval;
+
+//     function startSlider(){
+//         interval = setInterval(function(){
+//             $sliderContainer.animate({'margin-top': '-='+$slides.find('img').height()}, animationSpeed, function() {
+//                 currentSlide++;
+//             if(currentSlide == $slides.length ){
+//                 currentSlide = 1;
+//                 $sliderContainer.css('margin-top', 0);
+//                 }
+//             });
+//         }, pause);
+//     }
+//     function pauseSlider(){
+//         clearInterval(interval);
+//     }
+
+//     $slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
+// });
+
+
+
